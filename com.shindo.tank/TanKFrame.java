cbce4bec7d937f3493ab8fe716b2185572170df9
shindoyang <727792353@qq.com>
2020-05-28 23:11:33 +0800

    添加键盘处理，根据上下左右键判断坦克方向

--- com.shindo.tank/TanKFrame.java
+++ com.shindo.tank/TanKFrame.java
@@ -30,23 +30,61 @@ public class TanKFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
         g.fillRect(x, y, 50, 50);
-        x += 10;
+        //x += 10;
         //y += 10;
 
     }
 
     class MyKeyListener extends KeyAdapter {
+
+        boolean bl = false;
+        boolean bu = false;
+        boolean br = false;
+        boolean bd = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
             //x += 200;
-            //repaint(); //重画
+            //repaint();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
         }
 
 
